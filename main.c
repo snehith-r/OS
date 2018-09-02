6ab4f1cbd75ed6e08dc00c0a41284c5c56f637f2
snehith777 <35597622+snehith777@users.noreply.github.com>
2018-09-02 16:14:56 +0530

    Add files via upload

--- main.c
+++ main.c
@@ -5,6 +5,8 @@
 #include <sys/wait.h>
 #include <errno.h>
 #include <signal.h>
+#include "get_input.h"
+#include "execute.h"
 void getprompt()
 {
     char s[200],*user;
@@ -22,6 +24,9 @@ void shell_loop()
 {
     while(1)
     {
+        int stat;
+        while(waitpid(-1, &stat, WNOHANG) > 0);
+
         char home[1002];
         getcwd(home,1000);
         int home_len=strlen(home);
@@ -43,66 +48,64 @@ void shell_loop()
             for(int i=0;i<=pwd_len;i++) path[i]=pwd[i];
         }
 
-    char *username=getenv("USER"); 
-    char host[102];
-    gethostname(host,100);
-    printf(">%s%s@%s%s:%s%s%s","\x1B[1;34m",username,host,"\x1B[0m","\x1B[1;32m",path,"\x1B[0m");
-    
-    char* input=readline();
-    char** arg_array=parse(input,";");
-    for(int i=0;arg_array[i]!=NULL;++i)
-    {
-        if(strcmp(arg_array[i],"exit"))
+        char *username=getenv("USER"); 
+        char host[102];
+        gethostname(host,100);
+        printf(">%s%s@%s%s:%s%s%s","\x1B[1;34m",username,host,"\x1B[0m","\x1B[1;32m",path,"\x1B[0m");
+        
+        char* input=readline();
+        char** arg_array=parse(input,";");
+        for(int i=0;arg_array[i]!=NULL;++i)
         {
-            printf("\n");
-            exit(0);
-        }   
-
-        int p[2];
-        pid_t proc;
-        char** pipedargs=parse(arg_array[i],"|");
-        if(pipedargs[1]!=NULL)
-        {
-            for (int j=0;pipedargs[j]!=NULL;++j)
+            if(strcmp(arg_array[i],"exit")==0)
             {
-                int count=0;
-                char **args=parse(pipedargs[j]," \t\n\a\r");
-                
-                for(int i=0;args[i]!=NULL;++i)
-                count++;
-                pipe(p);
-                proc=fork();
-                if(proc==0)
+                exit(0);
+            }   
+
+            int p[2];
+            pid_t proc;
+            char** pipedargs=parse(arg_array[i],"|");
+            if(pipedargs[1]!=NULL)
+            {
+                for (int j=0;pipedargs[j]!=NULL;++j)
                 {
-                    dup2(p[0],0);
-                    if(pipedargs[j+1]!=NULL)dup2(p[1],1);
-                    close(p[0]);
-                    execute(args,count,home,home_len);
-                    exit(2);
+                    int count=0;
+                    char **args=parse(pipedargs[j]," \t\n\a\r");
+                    
+                    for(int i=0;args[i]!=NULL;++i)
+                    count++;
+                    pipe(p);
+                    proc=fork();
+                    if(proc==0)
+                    {
+                        dup2(p[0],0);
+                        if(pipedargs[j+1]!=NULL)dup2(p[1],1);
+                        close(p[0]);
+                        execute(args,count,home,home_len);
+                        exit(2);
+                    }
+                    else
+                    {
+                        wait(NULL);
+                        close(p[1]);
+                        i++;
+                    }                
                 }
-                else
-                {
-                    wait(NULL);
-                    close(p[1]);
-                    i++;
-                }                
-            }
-            
-        }
-
-        else
-        {
-                int count=0;
-                char **args=parse(arg_array[i]," \t\n\a\r");
                 
-                for(int i=0;args[i]!=NULL;++i)
-                count++;
-                execute(args,count,home,home_len);
+            }
 
-        }
+            else
+            {
+                    int count=0;
+                    char **args=parse(arg_array[i]," \t\n\a\r");
+                    for(int i=0;args[i]!=NULL;++i)
+                    count++;
+                    execute(args,count,home,home_len);
+
+            }
 
 
-    }
+         }
     
     }
 }
